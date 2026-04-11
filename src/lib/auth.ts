251c3695c1cbe830237d448705f479868e737cb8
Logan <e46logan@gmail.com>
2026-04-11 09:49:51 -0400

    Fixed auth origin error / hardcoded entry

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -65,6 +65,7 @@ export const auth = betterAuth({
 
   trustedOrigins: [
     process.env.BETTER_AUTH_URL || "http://localhost:3000",
+    process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
   ],
 });
 
